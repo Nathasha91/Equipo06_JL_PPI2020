205e2cf4b81b65a107594dc26bfb9670bf91a004
unknown <pafull.2003@gmail.com>
2020-10-16 11:52:58 -0500

    Direccioné los botones de tareas

--- Frontend/src/pages/Tarea/Tareas.js
+++ Frontend/src/pages/Tarea/Tareas.js
@@ -5,46 +5,46 @@ const Tareas = () => {
 <div className="m-5">
     <div className= "container">
         <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Física
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Química
             </a>
-            <a href="#" class="list-group-item list-group-item-action list-group-item-warning">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action list-group-item-warning">
                 Biología
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Inglés
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Lectora en Inglés
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Lengua Castellana
             </a>
-            <a href="#" class="list-group-item list-group-item-action list-group-item-warning">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action list-group-item-warning">
                 Emprendimiento
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Tecnología
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Educación Física
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Tiempo Libre
             </a>
-            <a href="#" class="list-group-item list-group-item-action list-group-item-warning">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action list-group-item-warning">
                 Filosofía
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Artística
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Economía y política
             </a>
-            <a href="#" class="list-group-item list-group-item-action">
+            <a href="/tareasHechas" class="list-group-item list-group-item-action">
                 Religión
             </a>
         </div>
